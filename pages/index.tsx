fb1ecaf486cd30acad1e0d45a14697b8bb57413f
Luis <luis16isasi@gmail.com>
2021-04-12 16:33:24 -0500

    chore: test tailwind :D

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 const Index = () => {
   return (
-    <div>
-      <h1 className="text-gray-700 ">DESDE INDEX</h1>
-    </div>
+    <>
+      {/* <div></div> */}
+      <nav className="bg-green-400 w-full h-20 flex justify-around items-center">
+        <h1 className="font-bold  text-xl">My Logo</h1>
+        <ul className="bg-red-400  w-1/2 flex flex-col justify-between sm:flex-row md:hidden">
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About us</li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
