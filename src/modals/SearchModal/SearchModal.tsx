5a137874b1c57ca12deb44a39a1ce320890216ce
Luis <luis16isasi@gmail.com>
2021-05-14 19:19:33 -0500

    fix: hide scroll when is open the search's modal

--- src/modals/SearchModal/SearchModal.tsx
+++ src/modals/SearchModal/SearchModal.tsx
@@ -25,51 +25,58 @@ const SearchModal: React.FC<Props> = ({ setIsOpenModal }) => {
   };
 
   return ReactDOM.createPortal(
-    <div className="bg-white z-50 max-h-screen w-screen h-auto absolute top-0 left-0 flex flex-col justify-start">
-      <div className=" flex-grow w-full overflow-y-auto scrool-none scroolbar">
-        <Container>
-          <HeaderModal handleBtn={handleBtn} />
-          <div className="box-border px-3 h-16 lg:h-36 mt-8 lg:mt-4 text-sm lg:text-lg">
-            <p className="font-semibold text-gray-700 ">
-              Los temas mas buscados:
-            </p>
-          </div>
-          {isMovilAndTablet && (
-            <div className="box-border px-3 h-20 ">
-              <FooterText />
-              <BtnToPremium />
+    <>
+      <div className="bg-white z-50 max-h-screen w-screen h-auto absolute top-0 left-0 flex flex-col justify-start">
+        <div className=" flex-grow w-full overflow-y-auto scrool-none scroolbar">
+          <Container>
+            <HeaderModal handleBtn={handleBtn} />
+            <div className="box-border px-3 h-16 lg:h-36 mt-8 lg:mt-4 text-sm lg:text-lg">
+              <p className="font-semibold text-gray-700 ">
+                Los temas mas buscados:
+              </p>
             </div>
-          )}
-          <p className=" box-border px-3 font-semibold text-gray-700 text-sm lg:text-lg my-4">
-            Los cursos más vendidos:
-          </p>
-          <div className="bg-transparent grid grid-flow-row grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-10 w-full h-auto">
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-            <CardCourse inModal={true} />
-          </div>
-        </Container>
+            {isMovilAndTablet && (
+              <div className="box-border px-3 h-20 ">
+                <FooterText />
+                <BtnToPremium />
+              </div>
+            )}
+            <p className=" box-border px-3 font-semibold text-gray-700 text-sm lg:text-lg my-4">
+              Los cursos más vendidos:
+            </p>
+            <div className="bg-transparent grid grid-flow-row grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-10 w-full h-auto">
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+              <CardCourse inModal={true} />
+            </div>
+          </Container>
+        </div>
+        {isMovilAndTablet === false && <FooterModal />}
       </div>
-      {isMovilAndTablet === false && <FooterModal />}
-    </div>,
+      <style global jsx>{`
+        body {
+          overflow: hidden;
+        }
+      `}</style>
+    </>,
     document.querySelector('#modal')
   );
 };
