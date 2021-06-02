d2da52ff36f4dbc51faf3976a3cad5907815c04c
Luis <luis16isasi@gmail.com>
2021-06-02 17:23:51 -0500

    style: add width in currency type

--- src/components/Footer/components/CurrencyType.tsx
+++ src/components/Footer/components/CurrencyType.tsx
@@ -4,7 +4,7 @@ import ExpandMoreIcon from '@Components/Icons/ExpandMoreIcon';
 
 const CurrencyType = () => {
   return (
-    <button className="w-full max-w-64  focus:outline-none border border-gray-500 rounded-md p-4 flex justify-between items-center">
+    <button className="w-full min-w-max  max-w-64 md:min-w-64  focus:outline-none border border-gray-500 rounded-md p-4 flex justify-between items-center">
       <div className="flex items-center text-gray-600">
         <Image src="images/PE-2.png" height="16" width="24" />
         <span className="font-bold mx-2">Perú</span>
