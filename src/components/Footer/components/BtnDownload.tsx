c8dc1efca8f68d9646b777667322e24a018f0885
Luis <luis16isasi@gmail.com>
2021-06-02 19:27:59 -0500

    style: change background color in button download

--- src/components/Footer/components/BtnDownload.tsx
+++ src/components/Footer/components/BtnDownload.tsx
@@ -1,6 +1,6 @@
 const BtnDownload: React.FC = ({ children }) => {
   return (
-    <button className="bg-base-dark dark:bg-base-light-dark-mode md:hover:bg-primary-lighter md:transition-colors md:duration-200 md:ease-in-out rounded-md max-w-80 md:min-w-56 py-3 text-lg text-white font-bold w-full mb-2 flex justify-center items-center">
+    <button className="bg-base-dark dark:bg-base-lighter-dark-mode md:hover:bg-primary-lighter md:transition-colors md:duration-200 md:ease-in-out rounded-md max-w-80 md:min-w-56 py-3 text-lg text-white font-bold w-full mb-2 flex justify-center items-center">
       {children}
     </button>
   );
